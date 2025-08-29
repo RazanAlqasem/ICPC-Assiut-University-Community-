cfaac74073dd860f793f3f16e06436e5d3fee06d
Razan majdy <razanwork2005@gmail.com>
2025-08-29 17:15:08 +0300

    Basic_Data_Types.java

---
+++ Sheet #1 (Data type - Conditions)/Basic_Data_Types.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+        int I = sc.nextInt();  
+        long  L = sc.nextLong();  
+        char c = sc.next().charAt(0);
+        float F = sc.nextFloat();  
+        double D = sc.nextDouble();
+      
+        System.out.println(I);
+        System.out.println(L);
+        System.out.println(c);
+        System.out.println(F);           
+        System.out.println(D);
+    }
+}
