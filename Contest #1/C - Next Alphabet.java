d148b0c3e2fdd0d47fe9c1f51586f6e1b5387a27
Razan majdy <razanwork2005@gmail.com>
2025-08-31 23:15:43 +0300

    C - Next Alphabet.java

---
+++ Contest #1/C - Next Alphabet.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0); 
+        if (c == 'z') {
+            System.out.println('a');
+        } else {
+            char next = (char)(c + 1); 
+            System.out.println(next);
+        }
+    }
+}
